c68b53dd6f7ebf19e7fb480f5fa3671d0bd195b6
Matthias Runge <mrunge@redhat.com>
2014-08-21 21:45:33 +0200

    Konfiguration

---
+++ pelicanconf.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'mrunge'
+SITENAME = u'Linux-Werkstatt Oldenburg'
+SITEURL = ''
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'de'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Treffen im Mainframe', 'http://mainframe.io/'),
+          ,)
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 3
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
